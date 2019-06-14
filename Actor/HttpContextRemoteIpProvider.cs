4eeffba76b35e46979d3c2c3fb4d38509519e228
Francois Hill <francoishill11@gmail.com>
2019-06-14 22:56:14 +0200

    feat: Add `HttpContextRemoteIpProvider : IRemoteIpProvider`

---
+++ Actor/HttpContextRemoteIpProvider.cs
@@ -0,0 +1,20 @@
+﻿using Firepuma.Api.Abstractions.Actor;
+using Microsoft.AspNetCore.Http;
+
+namespace Firepuma.Api.Common.Actor
+{
+    public class HttpContextRemoteIpProvider : IRemoteIpProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HttpContextRemoteIpProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetRemoteIp()
+        {
+            return _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+        }
+    }
+}
