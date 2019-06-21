4c5f186f02b7e1a656b3df1f5a634ce75819b099
Francois Hill <francoishill11@gmail.com>
2019-06-21 22:40:49 +0200

    feat: Improve `ConfigureAndValidate` by doing immediate validation
    The previous approach required us to actually still manually call `ValidateAll` after we called `ConfigureAndValidate`. This new approach is better and actually validates immediately.

--- src/Configure/ConfigExtensions.cs
+++ src/Configure/ConfigExtensions.cs
@@ -9,51 +9,37 @@ namespace Firepuma.Api.Common.Configure
 {
     public static class ConfigExtensions
     {
-        private static List<Action<IServiceProvider>> _validateActions = new List<Action<IServiceProvider>>();
-
         public static void ConfigureAndValidate<T>(this IServiceCollection serviceCollection, Action<T> configureOptions) where T : class, new()
         {
-            _validateActions.Add(serviceProvider =>
-            {
-                var options = serviceProvider.GetRequiredService<IOptions<T>>();
-                var unused = options.Value;
-            });
-
             // Inspired by https://blog.bredvid.no/validating-configuration-in-asp-net-core-e9825bd15f10
-            serviceCollection
-                .Configure(configureOptions)
-                .PostConfigure<T>(settings =>
+            serviceCollection.Configure(configureOptions);
+
+            using (var scope = serviceCollection.BuildServiceProvider().CreateScope())
+            {
+                var options = scope.ServiceProvider.GetRequiredService<IOptions<T>>();
+                var optionsValue = options.Value;
+                var configErrors = ValidationErrors(optionsValue).ToArray();
+                if (!configErrors.Any())
                 {
-                    var configErrors = settings.ValidationErrors().ToArray();
-                    if (configErrors.Any())
-                    {
-                        var aggregatedErrors = string.Join(",", configErrors);
-                        var count = configErrors.Length;
-                        var configType = typeof(T).FullName;
-                        throw new ApplicationException($"There are {count} configuration error(s) in {configType}: {aggregatedErrors}");
-                    }
-                });
+                    return;
+                }
+
+                var aggregatedErrors = string.Join(",", configErrors);
+                var count = configErrors.Length;
+                var configType = typeof(T).FullName;
+                throw new ApplicationException($"{configType} configuration has {count} error(s): {aggregatedErrors}");
+            }
         }
 
-        private static IEnumerable<string> ValidationErrors(this object @this)
+        private static IEnumerable<string> ValidationErrors(object obj)
         {
-            var context = new ValidationContext(@this, serviceProvider: null, items: null);
+            var context = new ValidationContext(obj, serviceProvider: null, items: null);
             var results = new List<ValidationResult>();
-            Validator.TryValidateObject(@this, context, results, true);
+            Validator.TryValidateObject(obj, context, results, true);
             foreach (var validationResult in results)
             {
                 yield return validationResult.ErrorMessage;
             }
-        }
-
-        public static void ValidateAll(IServiceProvider serviceProvider)
-        {
-            foreach (var action in _validateActions)
-            {
-                action(serviceProvider);
-            }
-
-            _validateActions = null;
         }
     }
 }
